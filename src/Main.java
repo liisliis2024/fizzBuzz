6f2c6088cbc699f892458afa0548ffa3830e1ef4
Liis <liisschotter@gmail.com>
2024-10-14 14:14:23 +0300

    First commit

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,25 @@
 public class Main {
     public static void main(String[] args) {
 
+        int number = 100;
+
+        for (int i = 1; i < number+1; i++) {
+
+//            3 & 5 fizzBuzz
+            if (i % 3 == 0 && i % 5 == 0 ) {
+                System.out.println("FizzBuzz");
+            }
+//            3 == fizz
+            else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            }
+//            5 == buzz
+            else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            }
+            else {
+                System.out.println(i);
+            }
+        }
     }
 }
