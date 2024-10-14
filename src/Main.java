3beef0c783ea6f45eef3c8c1921c4d11947d5c6a
Liis <liisschotter@gmail.com>
2024-10-14 14:51:28 +0300

    reformatted and improved naming

--- src/Main.java
+++ src/Main.java
@@ -3,20 +3,16 @@
 public class Main {
     public static void main(String[] args) {
 
-        int number = 100;
+        int count = 100;
 
-        for (int i = 1; i < number+1; i++) {
-
-            if (i % 3 == 0 && i % 5 == 0 ) {
+        for (int i = 1; i < count + 1; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
                 System.out.println("FizzBuzz");
-            }
-            else if (i % 3 == 0) {
+            } else if (i % 3 == 0) {
                 System.out.println("Fizz");
-            }
-            else if (i % 5 == 0) {
+            } else if (i % 5 == 0) {
                 System.out.println("Buzz");
-            }
-            else {
+            } else {
                 System.out.println(i);
             }
         }
